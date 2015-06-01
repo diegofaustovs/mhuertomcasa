1b8d10aaee24436276b5cb892d249eb1e83321e4
Diego Vanegas <diegofaustovs@gmail.com>
2015-06-01 13:24:02 -0500

    Bio, Elect, Gestion, Social en ES. Elect, Gestion en EN. Pendiente Social y Bio.

---
+++ resources/lang/en/equipo_electrico.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Diego F. Vanegas
+ * Date: 01/06/2015
+ * Time: 12:37 PM
+ */
---
+++ resources/lang/en/equipo_gestion.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Diego F. Vanegas
+ * Date: 01/06/2015
+ * Time: 12:53 PM
+ */
+
+return[
+
+    'equipo' => '',
+
+    'motivacion' => '',
+
+    'alejandro_echeverry.nombre' => 'Alejandro Echeverry',
+    'alejandro_echeverry.cargo' => '',
+
+    'maria_garcia.nombre' => 'María Alejandra García',
+    'maria_garcia.cargo' => '',
+
+    'edgar_chamorro.nombre' => 'Edgar Chamorro',
+    'edgar_chamorro.cargo' => '',
+
+    'tomas_mejia.nombre' => 'Tomás Mejía',
+    'tomas_mejia.cargo' => '',
+
+    'jorge_navia.nombre' => 'Jorge Navia',
+    'jorge_navia.cargo' => '',
+
+    'juan_sanchez.nombre' => 'Juan Camilo Sanchez',
+    'juan_sanchez.cargo' => '',
+
+];
