c12ac7458715d1980c6e9bc74f4f34984d709c04
Diego Vanegas <diegofaustovs@gmail.com>
2015-06-02 20:03:56 -0500

    Equipo Diseno, Biocl, Huerto EN - ES. Pendiente Social

---
+++ resources/lang/en/equipo_diseno.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Diego F. Vanegas
+ * Date: 02/06/2015
+ * Time: 6:40 PM
+ */
---
+++ resources/lang/en/equipo_huerto.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Diego F. Vanegas
+ * Date: 02/06/2015
+ * Time: 7:01 PM
+ */
