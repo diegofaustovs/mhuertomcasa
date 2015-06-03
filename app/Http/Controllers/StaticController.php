ee280f66d7c9b0de670b5a2eac742fd69fbc94d3
ss1993 <s.sierra470@uniandes.edu.co>
2015-06-02 19:28:06 -0500

    "Se agregan las rutas con /en y /es"

---
+++ app/Http/Controllers/StaticController.php
@@ -0,0 +1,25 @@
+<?php namespace App\Http\Controllers;
+
+class StaticController extends Controller {
+
+    /*
+    |--------------------------------------------------------------------------
+    | Static Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller renders every static page of the application
+    |
+    */
+
+
+    /**
+     * Show the application dashboard to the user.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+}
