1d15bfb6fbe7e36f403455218c03978c638e8eb6
marianaviro <marianaviro@hotmail.com>
2015-06-18 14:41:01 -0500

    Agregué info en español y alguna en inglés del equipo de Arquitectura

---
+++ resources/lang/es/equipo_arquitectura.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marianaviro
+ * Date: 6/18/15
+ * Time: 1:48 PM
+ */
+
+return [
+
+    'equipo' => 'EQUIPO DE ARQUITECTURA',
+
+    'motivacion' => 'Nuestra principal motivaci&oacute;n es dise&ntilde;ar una propuesta de vivienda sostenible de bajo presupuesto, que a diferencia de la vivienda econ&oacute;mica producida en Colombia, se enfatice en reconocer al usuario, reinterpretar sus costumbres habitacionales y ofrecerle una  vivienda digna. Basados en nuestra propuesta, creemos que la arquitectura tiene el deber de transformar la vida de quienes la habitan, haci&eacute;ndola no solamente m&aacute;s digna sino inclusive, mejor y m&aacute;s feliz. La calidad de vida deseada se logra mediante la reflexi&oacute;n de una arquitectura pensada para las personas, sus necesidades, circunstancias, contexto y costumbres. Una vez tenida en cuenta dicha premisa, ponemos nuestras habilidades y aprendizajes para proyectar espacios flexibles y habitables, solucionar t&eacute;cnica y materialmente los conceptos sostenibles, y desarrollar a fondo el concepto y la forma desde la escala urbana, hasta los detalles de la casa.',
+
+    'deca_1.nombre' => 'Juan Mario Pradilla',
+    'deca_1.cargo' => 'L&iacute;der de Urban Master Plan',
+
+    'deca_2.nombre' => 'Irene Arango',
+    'deca_2.cargo' => 'Integrante de Urban Master Plan',
+
+    'deca_3.nombre' => 'Ana Mar&iacute;a Vivas',
+    'deca_3.cargo' => 'Integrante de Urban Master Plan',
+
+    'deca_4.nombre' => 'Luis Miguel Narv&aacute;ez',
+    'deca_4.cargo' => 'Integrante de Urban Master Plan',
+
+    'deca_5.nombre' => 'Andr&eacute;s Felipe Cabal',
+    'deca_5.cargo' => 'L&iacute;der de Arquitectural Design',
+
+    'deca_6.nombre' => 'Santiago Palacio',
+    'deca_6.cargo' => 'Integrante de Arquitectural Design',
+
+    'deca_7.nombre' => 'Juan Pablo Bravo',
+    'deca_7.cargo' => 'Integrante de Arquitectural Design',
+
+    'deca_8.nombre' => 'Mar&iacute;a Camila &Aacute;lvarez',
+    'deca_8.cargo' => 'Integrante de Arquitectural Design',
+
+    'deca_9.nombre' => 'Daniela Vald&eacute;s',
+    'deca_9.cargo' => 'L&iacute;der de Interior Design',
+
+    'deca_10.nombre' => 'Manuela Isaza',
+    'deca_10.cargo' => 'Integrante de Interior Design',
+
+    'deca_11.nombre' => 'Alba Luc&iacute;a Olaya',
+    'deca_11.cargo' => 'Integrante de Interior Design',
+
+    'nintegrantes' => '11'
+
+];
