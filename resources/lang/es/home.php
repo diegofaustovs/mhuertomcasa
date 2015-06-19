44843162d9170b3b7edd06a24de1ecaea029cf33
marianaviro <marianaviro@hotmail.com>
2015-06-19 14:52:16 -0500

    Información en el home, lo de los Tweets, arreglos en el navbar y en el header

--- resources/lang/es/home.php
+++ resources/lang/es/home.php
@@ -8,6 +8,9 @@
 
 return [
     'explorar' => 'Explorar',
+    'whatTitle' => '&iquest;Qu&eacute; es MHMC?',
+    'whoTitle' => '&iquest;Qui&eacute;nes son MHMC?',
+    'by'=> 'de',
     'what' => 'La Universidad de los Andes se complace en participar en el SDLAC 2015 con el equipo M&aacute;s Huerto M&aacute;s Casa. Esta es una propuesta innovadora que busca ser una alternativa sostenible para las personas que esperan vivir en viviendas de los estratos medios y bajos, por medio de una aldea ecol&oacute;gica y en gran parte auto sostenible econ&oacute;micamente. Que no solo se proyecte como un lugar de vivienda digna sino que brinde oportunidades econ&oacute;micas para sus habitantes al disminuir los costos y generar ingresos para el funcionamiento de la comunidad.',
     'who' => 'Con el fin de elaborar una propuesta integral, formamos un equipo con los mejores estudiantes de diferentes programas, igualmente contamos con el patrocinio de Emgesa, Codensa y el Grupo Enel que es l&iacute;der en la distribuci&oacute;n de energ&iacute;a el&eacute;ctrica en Bogot&aacute; y Cundinamarca, Colombia. Esta alianza academia – empresa es plenamente capaz de proponer una soluci&oacute;n disruptiva, sostenible con gran impacto social para Am&eacute;rica Latina y el Caribe.'
 ];
