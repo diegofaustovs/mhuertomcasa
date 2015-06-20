88b4be2253ab0b4890284d8fa47e8aa16800b178
marianaviro <marianaviro@hotmail.com>
2015-06-20 00:27:02 -0500

    Agregué header en todas las páginas

--- resources/views/teams/org_chart.blade.php
+++ resources/views/teams/org_chart.blade.php
@@ -1,15 +1,21 @@
 @extends('index')
 
 @section('content')
+    <body class="no-sidebar">
 
-    @include('header')
+    <div id="page-wrapper">
 
-    <div class="wrapper style1">
-        <div id="titulo">
-            <h2>{{Lang::get('miscellaneous.org_chart')}}</h2>
+
+        <div id="header">
+            @include('header')
         </div>
-        <div class="container">
-            <img style="width: 100%" src="{{asset('images/organigrama.png')}}">
+        <div class="wrapper style1">
+            <div id="titulo">
+                <h2>{{Lang::get('miscellaneous.org_chart')}}</h2>
+            </div>
+            <div class="container">
+                <img style="width: 100%" src="{{asset('images/organigrama.png')}}">
+            </div>
         </div>
     </div>
 @endsection('content')
