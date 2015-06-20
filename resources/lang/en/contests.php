8f1fab602f9014853ca8aed45806dbb69353f6e1
ss1993 <s.sierra470@uniandes.edu.co>
2015-06-20 01:05:54 -0500

    Se corrigen los links:hover, se agregan los lang del concurso y el rutaje

---
+++ resources/lang/en/contests.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marianaviro
+ * Date: 6/18/15
+ * Time: 1:48 PM
+ */
+
+return [
+
+    'title' => 'Contests',
+
+    'description'  => 'The Decathlon is based on a careful evaluation of 10 contests with a total of 1000 points.
+                        600 points are given by the jury an the other 400 points come from measurable contests',
+
+    'architecture.title' =>'Architecture',
+    'eng-const.title'=>'Engineering &amp; Construction',
+    'energy.title'=> 'Energy Efficiency',
+    'energy-consum.title' => 'Electrical Energy Balance',
+    'comfort.title' => 'Comfort',
+    'sustainability.title' => 'Sustainability',
+    'house-func.title' => 'House Functioning',
+    'marketing.title' => 'Marketing, Communication &amp; Social Awareness',
+    'urban-design.title' => 'Urban Design &amp; Affordability',
+    'innovation.title' => 'Innovation'
+
+];
---
+++ resources/views/solar/contests.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebastian
+ * Date: 20/06/15
+ * Time: 1:04
+ */
