64c60e0a118876345d76f7d5f5275fcf44e19c6c
Diego Vanegas <diegofaustovs@gmail.com>
2015-10-16 10:56:51 -0500

    Fotos - Comunicaciones

--- resources/lang/es/equipo_diseno.php
+++ resources/lang/es/equipo_diseno.php
@@ -12,33 +12,39 @@ return[
 
     'motivacion' => 'Nos motiva el poder poner en pr&aacute;ctica las herramientas que hemos adquirido por medio de las distintas oportunidades y actividades a lo largo de nuestra vida, con el fin de responder a la responsabilidad social que tenemos frente a las personas que no han tenido la posibilidad de desarrollarse bajo las mismas condiciones. Queremos trabajar para conseguir un dise&ntilde;o de interior y un plan de comunicaciones que evidencia nuestro inter&eacute;s y esfuerzo por la sostenibilidad, la igualdad social y la eficiencia energ&eacute;tica.',
 
-    'deca_1.nombre' => 'Ver&oacute;nica Posada',
-    'deca_1.cargo' => 'L&iacute;der de Grupo - Dise&ntilde;o de interiores',
+    'deca_1.nombre' => 'Laura Esguerra',
+    'deca_1.cargo' => 'L&iacute;der de Grupo - Docum. del Proyecto',
+    'deca_1.foto' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_9604.JPG',
 
-    'deca_2.nombre' => 'Laura Esguerra',
-    'deca_2.cargo' => 'L&iacute;der de Grupo - Docum. del Proyecto',
+    'deca_2.nombre' => 'Juan Diego Santos',
+    'deca_2.cargo' => 'L&iacute;der de Grupo - Audiovisuales y Gr&aacute;fica',
+    'deca_2.foto' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_9604.JPG',
 
-    'deca_3.nombre' => 'Juan Diego Santos',
-    'deca_3.cargo' => 'L&iacute;der de Grupo - Audiovisuales y Gr&aacute;fica',
+    'deca_3.nombre' => 'Diego Vanegas',
+    'deca_3.cargo' => 'L&iacute;der de Grupo - Desarrollo Web',
+    'deca_3.foto' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_9732.JPG',
 
-    'deca_4.nombre' => 'Daniela Vald&eacute;s',
-    'deca_4.cargo' => 'Encargada de planos y cortes',
+    'deca_4.nombre' => 'Isabel Arango',
+    'deca_4.cargo' => 'Diagramaci&oacute;n e identidad visual del equipo',
+    'deca_4.foto' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_9642.JPG',
 
-    'deca_5.nombre' => 'Alba Luc&iacute;a Olaya',
-    'deca_5.cargo' => 'Dise&ntilde;o de sillas plegables y c&eacute;lulas',
+    'deca_5.nombre' => 'Mar&iacute;a Andrea Gonz&aacute;lez',
+    'deca_5.cargo' => 'Audiovisuales y Gr&aacute;fica',
+    'deca_5.foto' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_9667.JPG',
 
-    'deca_6.nombre' => 'Manuela Isaza',
-    'deca_6.cargo' => 'L&iacute;der de Grupo - Dise&ntilde;o de mecanismos de apertura de muebles plegables',
+    'deca_6.nombre' => 'Andrea Peralta',
+    'deca_6.cargo' => 'Diagramaci&oacute;n e identidad visual del equipo',
+    'deca_6.foto' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_9661.JPG',
 
-    'deca_7.nombre' => 'Isabel Arango',
-    'deca_7.cargo' => 'Diagramaci&oacute;n e identidad visual del equipo',
+    'nintegrantes' => '6',
 
-    'deca_8.nombre' => 'Mar&iacute;a Andrea Gonz&aacute;lez',
-    'deca_8.cargo' => 'Audiovisuales y Gr&aacute;fica',
+    'f_banner_1' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_9670.JPG',
+    'f_banner_2' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_8551.png',
+    'f_banner_3' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_8553.png',
+    'f_banner_4' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_8555.png',
+    'f_banner_5' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_8557.png',
+    'f_banner_6' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_8558.png',
 
-    'deca_9.nombre' => 'Andrea Peralta',
-    'deca_9.cargo' => 'Diagramaci&oacute;n e identidad visual del equipo',
-
-    'nintegrantes' => '9'
+    'n_f_banner' => '6',
 
 ];
