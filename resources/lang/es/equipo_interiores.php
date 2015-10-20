9cc0f0adb689fcdab8075f7cae697a6b258098e4
Diego Vanegas <diegofaustovs@gmail.com>
2015-10-20 17:54:45 -0500

    Interiores - ES

--- resources/lang/es/equipo_interiores.php
+++ resources/lang/es/equipo_interiores.php
@@ -2,24 +2,24 @@
 
 return[
 
-    'equipo' => 'Interior Design Team',
+    'equipo' => 'Diseño de Interiores',
 
-    'motivacion' => 'Our main objective is to share the work and activities made by our teammates, in order to show our motivation for this project and the progress we have achieved in seeking ways to build a better future. Similarly, we spread news about different projects that share our passion towards reducing human impact in our natural environment. We are able to fulfill this task through our social networks (i.e. Facebook) and our webpage. As a result, our efforts help raise environmental awareness in the communities we are involved in.',
+    'motivacion' => '',
 
     'deca_1.nombre' => 'Manuela Isaza',
-    'deca_1.cargo' => 'Team Leader',
+    'deca_1.cargo' => 'Lider de Equipo',
     'deca_1.foto' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_9646.JPG',
 
     'deca_2.nombre' => 'Alba Olaya',
-    'deca_2.cargo' => 'Decathlete',
+    'deca_2.cargo' => 'Decatleta',
     'deca_2.foto' => 'https://s3-us-west-2.amazonaws.com/fausto-mhmc/IMG_9691.JPG',
 
     'deca_3.nombre' => 'Daniela Valdes',
-    'deca_3.cargo' => 'Decathlete',
+    'deca_3.cargo' => 'Decatleta',
     'deca_3.foto' => '',
 
     'deca_4.nombre' => 'Veronica Posada',
-    'deca_4.cargo' => 'Decathlete',
+    'deca_4.cargo' => 'Decatleta',
     'deca_4.foto' => '',
 
     'nintegrantes' => '4'
